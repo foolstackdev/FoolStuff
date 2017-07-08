f7e7afcc19d8a9a1a548306eeb1c9a275cc52298
palmer eldritch <alonpalmer@outlook.it>
2017-07-08 11:22:52 +0200

    New data model to implement as fork for data testing. New EmployeesController associated to the persistence layer

---
+++ FoolStuff/Controllers/EmployeesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace FoolStuff.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+
+    }
+}
