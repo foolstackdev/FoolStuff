af8cece31248784591f361cd7687e983f232698f
palmer eldritch <alonpalmer@outlook.it>
2017-07-14 00:02:42 +0200

    Add in backend new Entity for user and account management, add new controller to manage user account. Add in client new view and controller and constant to manage user registration

---
+++ FoolStuff/Controllers/UserAccountController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FoolStaffDataAccess;
+
+namespace FoolStuff.Controllers
+{
+    [RoutePrefix("api/useraccount")]
+    public class UseraccountController : ApiController
+    {
+        [HttpGet]
+        [Route("isAlive")]
+        public HttpResponseMessage isAlive()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public HttpResponseMessage Register([FromBody]User user)
+        {
+            try
+            {
+                using (FoolStaffDataModelContainer entities = new FoolStaffDataModelContainer())
+                {
+                    entities.Users.Add(user);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, user);
+                    message.Headers.Location = new Uri(Request.RequestUri + user.Id.ToString());
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+
+    }
+}
