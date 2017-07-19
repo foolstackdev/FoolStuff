aa9f51e0b4fb75554f21e45ec48976547e010e82
palmer eldritch <alonpalmer@outlook.it>
2017-07-19 19:22:37 +0200

    Add basic authentication in Backend (not finished) Add Toaster in front end

---
+++ FoolStuff/UsersSecurity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FoolStaffDataAccess;
+
+namespace FoolStuff
+{
+    public class UsersSecurity
+    {
+        public static bool Login(string username, string password)
+        {
+            using(FoolStaffDataModelContainer entities = new FoolStaffDataModelContainer())
+            {
+                return entities.Users.Any(user => user.EMAIL.Equals(username, StringComparison.OrdinalIgnoreCase) && user.PASSWORD == password);
+            }
+        }
+
+
+    }
+}
