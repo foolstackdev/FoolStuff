b354dfb660bc68721eb7f25c7ec138d69d0df5d9
palmer eldritch <alonpalmer@outlook.it>
2017-11-25 13:01:34 +0100

    refactor

--- FoolStuffClient/app/controller/task.controller.js
+++ FoolStuffClient/app/controller/task.controller.js
@@ -33,6 +33,7 @@ angular
             vm.tasklist = response.data;
             toastr.success('Task inserted', 'Confirmed');
             $state.reload();
+
         }, function (err) {
             console.log(err)
             toastr.error('Problems during task insert', 'Something went wrong [' + err.data.exceptionMessage + ']');
